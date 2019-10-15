cf5b9cc2ac398b7fe992639742c573d825f2cbb5
rbenner1184 <rbenner1184@gmail.com>
2019-10-15 19:52:32 +0000

    Done with Day 1

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,21 @@
 
 import './styles.css';
-console.log('Ready to Party With Some TypeScript!');
+import { multiply } from './math';
+
+
+const number1E1 = document.getElementById('number1') as HTMLInputElement;
+const number2E1 = document.getElementById('number2') as HTMLInputElement;
+const mutiplyButton = document.getElementById('multiply') as HTMLInputElement;
+const answerE1 = document.getElementById('answer') as HTMLInputElement;
+
+mutiplyButton.addEventListener('click', function () {
+
+    const n1 = number1E1.valueAsNumber;
+    const n2 = number2E1.valueAsNumber;
+
+    const answer = multiply(n1, n2); // TODO: Put this in module.
+
+    answerE1.innerHTML = answer.toString();
+
+});
 
